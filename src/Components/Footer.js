90fddad3ff8bacbaf8abcea90dc649623a07e58d
Abhinand <websollabsabhinand@gmail.com>
2023-05-04 14:03:07 +0530

    footer-changed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './Footer.module.css'
 import logo from '../Assets/logo.png'
-import phone_logo from '../Assets/phone.png'
-import mail_logo from '../Assets/mail.png'
+import phone_logo from '../Assets/phone.svg'
+import mail_logo from '../Assets/mail.svg'
 import location_logo from '../Assets/location.svg'
 import Facebook from '../Assets/facebook.svg'
 import Instagram from '../Assets/instagram.svg'
@@ -21,23 +21,29 @@ const Footer = () => {
           </div>
           <div className={styles.mainFooter_div_Flex_2}>
             <div className={styles.location_flex_div} style={{display:'flex',alignItems:'center'}}>
-              <div style={{}}>
-                <img src={location_logo} style={{height:'70px',width:'70px'}}></img>
+              <div style={{width:'15%'}}>
+                <img src={location_logo} style={{height:'50px',width:'50px'}}></img>
               </div>
-              <p style={{overflowWrap:'break-word',marginLeft:'1rem'
-            }}>Amrita Vishwa Vidyapeetham,Amritapuri, Clappana PO,Kollam - 690525, Kerala, India</p>
+              <div style={{width:'85%'}}>
+                <p style={{overflowWrap:'break-word',paddingLeft:'5px'
+              }}>Amrita Vishwa Vidyapeetham,Amritapuri, Clappana PO,Kollam - 690525, Kerala, India</p>
+              </div>
             </div>
             <div className={styles.phone_flex_div} style={{display:'flex',marginTop:'1rem',alignItems:'center'}}>
-              <div style={{marginRight:'1rem'}}>
-                <img src={phone_logo} alt='location logo' style={{width:'100%',objectFit:'contain'}}></img>
+              <div style={{width:'15%'}}>
+                <img src={phone_logo} alt='location logo' style={{height:'50px',width:'50px'}}></img>
               </div>
-              <p style={{alignItems:'center'}}>9747030712, 9567979111.</p>
+              <div style={{width:'85%'}}>
+                <p style={{alignItems:'center'}}>&nbsp;9747030712, 9567979111.</p>
+              </div>
             </div>
             <div className={styles.phone_flex_div} style={{display:'flex',marginTop:'1rem',alignItems:'center'}}>
-              <div style={{}}>
-                <img src={mail_logo} alt='location logo' style={{width:'100%',objectFit:'contain'}}></img>
+              <div style={{width:'15%'}}>
+                <img src={mail_logo} alt='location logo' style={{height:'50px',width:'50px'}}></img>
               </div>
-              <p style={{marginLeft:'1rem'}}>contact@amritaedge.com</p>
+              <div style={{width:'85%'}}>
+                <p>&nbsp;contact@amritaedge.com</p>
+              </div>
             </div>
           </div>
           <div className={styles.mainFooter_div_Flex_3}>
